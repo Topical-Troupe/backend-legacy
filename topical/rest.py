a5db7492bfed88bd7248aefad24be02735849791
Britton W <britton.wolfe1999@gmail.com>
2020-07-17 14:59:03 -0400

    made migrations and added endpoints

--- topical/rest.py
+++ topical/rest.py
@@ -1,11 +1,21 @@
 from rest_framework import routers, viewsets
+from rest_framework.decorators import action
 
 from .models import User
 from .serializers import UserSerializer
 
 router = routers.DefaultRouter()
 
-class UserSerializer(viewsets.ModelViewSet):
+class UserViewSet(viewsets.ModelViewSet):
 	queryset = User.objects.all()
 	serializer_class = UserSerializer
 	lookup_field = 'username'
+	@action(detail = False, methods = ['GET'])
+	def me(self, request, username):
+		serializer = UserSerializer(
+			request.user,
+			many = False,
+			context = { 'context': request }
+		)
+		return Response(serializer.data)
+router.register('user', UserViewSet)
--- topical/urls.py
+++ topical/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
 	2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
-from .rest import router
+from .rest import router, UserViewSet
 
 urlpatterns = [
 	path('admin/', admin.site.urls),
 
-	path('api/', include(router.urls))
+	path('api/', include(router.urls)),
+	path('api/me/', UserViewSet.as_view({ 'get': 'me' }), name = 'me')
 ]
