5944d3d03e3029d40589f235848c21a80625d362
Britton W <britton.wolfe1999@gmail.com>
2020-07-17 15:02:43 -0400

    fixed some minor errors

--- topical/rest.py
+++ topical/rest.py
@@ -1,5 +1,7 @@
+from django.http import HttpResponse
 from rest_framework import routers, viewsets
 from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .models import User
 from .serializers import UserSerializer
@@ -11,7 +13,9 @@ class UserViewSet(viewsets.ModelViewSet):
 	serializer_class = UserSerializer
 	lookup_field = 'username'
 	@action(detail = False, methods = ['GET'])
-	def me(self, request, username):
+	def me(self, request):
+		if not request.user.is_authenticated:
+			return HttpResponse(status = 404)
 		serializer = UserSerializer(
 			request.user,
 			many = False,
