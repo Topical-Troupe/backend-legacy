596c900fc09fc432acc17a52f7b03076e650a072
Britton <britton.wolfe1999@gmail.com>
2020-07-18 15:31:46 -0400

    added a UPC field and changed the slug() method to generate_slug() to avoid confusion

--- topical/models.py
+++ topical/models.py
@@ -7,6 +7,7 @@ MAX_NAME_LEN = 512
 class Product(models.Model):
 	name = models.CharField(max_length = MAX_NAME_LEN)
 	description = models.TextField(max_length = MAX_DESCRIPTION_LEN, null = True)
+	upc = models.IntegerField(null = True)
 
 class Ingredient(models.Model):
 	name = models.CharField(max_length = MAX_NAME_LEN)
@@ -14,11 +15,11 @@ class Ingredient(models.Model):
 	description = models.TextField(max_length = MAX_DESCRIPTION_LEN, null = True)
 	in_products = models.ManyToManyField(to = Product, symmetrical = True, related_name = 'ingredients')
 	def save(self, *args, **kwargs):
-		self.slug = self.slug()
+		self.slug = self.generate_slug()
 		super(Ingredient, self).save(*args, **kwargs)
 	def __str__(self):
 		return self.name
-	def slug(self):
+	def generate_slug(self):
 		return (self.name.lower().replace(' ', '_'))
 
 class IngredientName(models.Model):
