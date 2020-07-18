efc17a6daea8f3acd7e3605a205a99c1af53cf76
Britton W <britton.wolfe1999@gmail.com>
2020-07-17 21:23:24 -0400

    created product model, serializer, related fields, and migrations

--- topical/models.py
+++ topical/models.py
@@ -2,15 +2,20 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 MAX_DESCRIPTION_LEN = 8192
-MAX_INGREDIENT_LEN = 512
+MAX_NAME_LEN = 512
+
+class Product(models.Model):
+	name = models.CharField(max_length = MAX_NAME_LEN)
+	description = models.TextField(max_length = MAX_DESCRIPTION_LEN, null = True)
 
 class Ingredient(models.Model):
-	name = models.CharField(max_length = MAX_INGREDIENT_LEN)
-	description = models.TextField(max_length = MAX_DESCRIPTION_LEN)
+	name = models.CharField(max_length = MAX_NAME_LEN)
+	description = models.TextField(max_length = MAX_DESCRIPTION_LEN, null = True)
+	in_products = models.ForeignKey(to = Product, on_delete = models.SET_NULL, null = True, related_name = 'ingredients')
 
 class IngredientName(models.Model):
-	ingredient = models.ForeignKey(to = Ingredient, on_delete = models.CASCADE, related_name = 'names')
-	name = models.CharField(max_length = MAX_INGREDIENT_LEN)
+	ingredient = models.ForeignKey(to = Ingredient, on_delete = models.SET_NULL, null = True, related_name = 'names')
+	name = models.CharField(max_length = MAX_NAME_LEN)
 	def __repr__(self):
 		return self.name
 
--- topical/serializers.py
+++ topical/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Ingredient, User
+from .models import Ingredient, Product, User
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
 	class Meta:
@@ -11,3 +11,8 @@ class IngredientSerializer(serializers.HyperlinkedModelSerializer):
 	class Meta:
 		model = Ingredient
 		fields = ['names', 'description']
+
+class ProductSerializer(serializers.HyperlinkedModelSerializer):
+	class Meta:
+		model = Product
+		fields = ['name', 'description', 'ingredients']
