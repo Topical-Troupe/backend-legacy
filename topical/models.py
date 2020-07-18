a8a0efd5a548c70dd29276cc5607e3c9c3d4ff33
Britton W <britton.wolfe1999@gmail.com>
2020-07-17 21:41:36 -0400

    changed the in_product field to m2m and added slugs to the ingredient model

--- topical/models.py
+++ topical/models.py
@@ -10,13 +10,21 @@ class Product(models.Model):
 
 class Ingredient(models.Model):
 	name = models.CharField(max_length = MAX_NAME_LEN)
+	slug = models.CharField(max_length = MAX_NAME_LEN)
 	description = models.TextField(max_length = MAX_DESCRIPTION_LEN, null = True)
-	in_products = models.ForeignKey(to = Product, on_delete = models.SET_NULL, null = True, related_name = 'ingredients')
+	in_products = models.ManyToManyField(to = Product, symmetrical = True, related_name = 'ingredients')
+	def save(self, *args, **kwargs):
+		self.slug = self.slug()
+		super(Ingredient, self).save(*args, **kwargs)
+	def __str__(self):
+		return self.name
+	def slug(self):
+		return (self.name.lower().replace(' ', '_'))
 
 class IngredientName(models.Model):
 	ingredient = models.ForeignKey(to = Ingredient, on_delete = models.SET_NULL, null = True, related_name = 'names')
 	name = models.CharField(max_length = MAX_NAME_LEN)
-	def __repr__(self):
+	def __str__(self):
 		return self.name
 
 class User(AbstractUser):
