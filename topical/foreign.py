41b565d3a69b1e7b289af700a47b9e13374c43a8
Britton W <britton.wolfe1999@gmail.com>
2020-07-19 17:30:37 -0400

    started implementing foreign database fetching

---
+++ topical/foreign.py
@@ -0,0 +1,21 @@
+from requests import get
+from django.http import HttpResponse
+
+from .models import Product
+
+FAPI_URL = 'https://api.upcitemdb.com/prod/trial/lookup?upc='
+
+def get_product_info(upc):
+	url = f'{FAPI_URL}{upc}'
+	return get(url)
+
+def get_product_or_create(upc):
+	item = Product.objects.filter(upc = upc)
+	if len(item) == 0:
+		response = get_product_info(upc)
+		if response.status_code != 200:
+			return HttpResponse(status = response.status_code)
+		item = Product()
+		item.name = response.json['title']
+		raw_ing = response.json['description']
+		return item
