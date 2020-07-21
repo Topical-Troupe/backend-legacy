0254f8769e51cd02b93d8981edbbfdbad846fab7
Britton W <britton.wolfe1999@gmail.com>
2020-07-20 21:05:54 -0400

    fixed a missing import

--- topical/rest.py
+++ topical/rest.py
@@ -4,7 +4,7 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from .models import Ingredient, Product, User
-from .serializers import UserSerializer
+from .serializers import IngredientSerializer, ProductSerializer, UserSerializer
 
 router = routers.DefaultRouter()
 
