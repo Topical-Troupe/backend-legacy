a060f522bb280d59c38f8de9398ee443df885c05
Britton W <britton.wolfe1999@gmail.com>
2020-07-27 18:58:37 -0400

    got the tags search to work

--- topical/views.py
+++ topical/views.py
@@ -30,9 +30,9 @@ def search_products(request):
     else:
         excluded_ingredients = User.get_default_exclusions()
     if name_q is not None:
-        #tags = Tag.objects.filter(name__contains = name_q.split())
+        tags = Tag.objects.filter(name__contains = name_q.split())
         products = Product.objects.filter(
-            #Q(tags__name__in = tags) |
+            Q(tags__in = tags) |
             Q(name__search = name_q) |
             Q(description__search = name_q) |
             Q(ingredients__names__name__search = name_q)
