3298fbda4344da7a46d70533ea90fba24a7cf452
Britton W <britton.wolfe1999@gmail.com>
2020-07-27 13:46:33 -0400

    dropped the ingredients field on the serializer

--- topical/serializers.py
+++ topical/serializers.py
@@ -49,7 +49,6 @@ class IngredientNameSerializer(serializers.HyperlinkedModelSerializer):
 		fields = ['name', 'ingredient']
 
 class ProductSerializer(serializers.HyperlinkedModelSerializer):
-	ingredients = IngredientSerializer(read_only = True, many = True, required = False, default = [])
 	class Meta:
 		model = Product
 		fields = ['name', 'upc', 'image_url', 'description'] 
