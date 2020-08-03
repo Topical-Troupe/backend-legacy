19936ed90a7a30b82ddcd0c99936c0a6c517c2a6
Britton W <britton.wolfe1999@gmail.com>
2020-08-02 22:21:57 -0400

    updated permissions

--- topical/settings.py
+++ topical/settings.py
@@ -150,7 +150,7 @@ AUTH_USER_MODEL = 'topical.User'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
